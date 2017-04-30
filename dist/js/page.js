724a3adbbe5a2e9c2848eeca273584ba75129b49
ereboz <ereboz8@gmail.com>
2017-04-30 15:02:26 -0700

    some text title additions for each section

--- dist/js/page.js
+++ dist/js/page.js
@@ -2,16 +2,10 @@
  * Created by javaBoxer on 4/27/2017.
  */
 
-console.log("Brian Test2");
-
 $(document).ready(function(){
-    
     // Load Header and Footer
     $(function(){
         $(".M04-Header").load("http://localhost:63342/coastalPhlebotomy/dist/Modules/M04-Header/partial.html");
         $(".M05-Footer").load("http://localhost:63342/coastalPhlebotomy/dist/Modules/M05-Footer/partial.html");
     });
-    
-
-
 });
