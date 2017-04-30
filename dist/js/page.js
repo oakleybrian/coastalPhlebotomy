d1ef0e2fd1a768b81b58f0f6d4c05497297b5f48
ereboz <ereboz8@gmail.com>
2017-04-30 13:07:26 -0700

    Added: Home, Services, About, Contact HTML pages. Added M04-Header, M05-Footer MODULES to keep the dir structure tight.

--- dist/js/page.js
+++ dist/js/page.js
@@ -2,6 +2,14 @@
  * Created by javaBoxer on 4/27/2017.
  */
 
+console.log("terst frm brian");
+
+// Load Header and Footer
+$(function(){
+    $(".M04-Header").load("dist/Modules/M04-Header/partial.html");
+    $(".M05-Footer").load("dist/Modules/M05-Footer/partial.html");
+});
+
 // affixes top nav to be sticky
 $('#topnavbar').affix({
   offset: {
