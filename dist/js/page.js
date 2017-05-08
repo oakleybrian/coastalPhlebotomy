045e3695a954bc1ea671a1eaaef71db22880e26a
ereboz <ereboz8@gmail.com>
2017-05-07 20:49:13 -0700

    Removed header and footer as modules

--- dist/js/page.js
+++ dist/js/page.js
@@ -3,9 +3,18 @@
  */
 
 $(document).ready(function(){
-    // Load Header and Footer
-    $(function(){
-        $(".M04-Header").load("dist/Modules/M04-Header/partial.html");
-        $(".M05-Footer").load("dist/Modules/M05-Footer/partial.html");
+    // affixes top nav to be sticky
+    $('#topnavbar').affix({
+        offset: {
+            top: $('#banner').height()
+        }
     });
 });
+
+// $(document).ready(function(){
+//     // Load Header and Footer
+//     $(function(){
+//         $(".M04-Header").load("dist/Modules/M04-Header/partial.html");
+//         $(".M05-Footer").load("dist/Modules/M05-Footer/partial.html");
+//     });
+// });
