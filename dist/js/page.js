7e4aa4f79148056ba325b7ae6ee00b6971f7d5c7
ereboz <ereboz8@gmail.com>
2017-05-07 18:24:45 -0700

    Removed local paths and DEPLOYED to coastphlebotomy.com

--- dist/js/page.js
+++ dist/js/page.js
@@ -5,7 +5,9 @@
 $(document).ready(function(){
     // Load Header and Footer
     $(function(){
-        $(".M04-Header").load("http://localhost:63342/coastalPhlebotomy/dist/Modules/M04-Header/partial.html");
-        $(".M05-Footer").load("http://localhost:63342/coastalPhlebotomy/dist/Modules/M05-Footer/partial.html");
+        // $(".M04-Header").load("http://localhost:63342/coastalPhlebotomy/dist/Modules/M04-Header/partial.html");
+        // $(".M05-Footer").load("http://localhost:63342/coastalPhlebotomy/dist/Modules/M05-Footer/partial.html");
+        $(".M04-Header").load("dist/Modules/M04-Header/partial.html");
+        $(".M05-Footer").load("dist/Modules/M05-Footer/partial.html");
     });
 });
