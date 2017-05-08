508e4f9b2a0564b216c3dc3c3c6d60252f98e1ba
ereboz <ereboz8@gmail.com>
2017-05-07 19:29:56 -0700

    Removed .container widht of 1012px. Added wider images for slideshow

--- dist/js/page.js
+++ dist/js/page.js
@@ -5,8 +5,6 @@
 $(document).ready(function(){
     // Load Header and Footer
     $(function(){
-        // $(".M04-Header").load("http://localhost:63342/coastalPhlebotomy/dist/Modules/M04-Header/partial.html");
-        // $(".M05-Footer").load("http://localhost:63342/coastalPhlebotomy/dist/Modules/M05-Footer/partial.html");
         $(".M04-Header").load("dist/Modules/M04-Header/partial.html");
         $(".M05-Footer").load("dist/Modules/M05-Footer/partial.html");
     });
